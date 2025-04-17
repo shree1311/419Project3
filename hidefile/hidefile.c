2a2772ec345c9d13a4ee2da4d9cbecf5a6baabca
Shreerang Patel <shreerang.patel@rutgers.edu>
2025-04-17 14:02:57 -0400

    I think it should be done

--- hidefile/hidefile.c
+++ hidefile/hidefile.c
@@ -7,6 +7,8 @@
 #include <errno.h>	// for setting an error code
 #include <dlfcn.h>	// for dynamic loading
 
+static int (*real_open) (const char *pathname, int flags, ...);
+static struct dirent *(*real_readdir)(DIR *dirp);
 
 // interpose the open call
 // open takes an optional third argument - the permissions for creating a file
@@ -14,9 +16,42 @@
 //	int open(const char *pathname, int flags)
 //	int open(const char *pathname, int flags, mode_t mode)
 //
+
+static int contains_any(const char *str, const char *list){
+	if (!list || !str) return 0;
+
+	char *list_copy = strdup(list);
+	char *token = strtok(list_copy, ":");
+
+	while (token) {
+		if (strstr(str, token)){
+			free(list_copy);
+			return 1;
+		}
+		token = strtok(NULL, ":");
+	}
+	
+	free(list_copy);
+	return 0;
+}
 int open(const char *pathname, int flags, ...)
 {
-	// your code here
+	if (!real_open) {
+		real_open = dlsym(RTLD_NEXT, "open");
+	}
+
+	//need to check if file needs to be blocked
+	if (contains_any(pathname, getenv("BLOCKED"))){
+		errno = EACCES;
+		return -1;
+	}
+	
+	va_list args;
+	va_start(args, flags);
+	int mode  = va_arg(args, int);
+	va_end(args);
+
+	return real_open(pathname, flags, mode);
 }
 
 // interpose the readdir call
@@ -25,5 +60,16 @@ int open(const char *pathname, int flags, ...)
 
 struct dirent *readdir(DIR *dirp)
 {
-	// your code here
+	if (!real_readdir){
+		real_readdir = dlsym(RTLD_NEXT, "readdir");
+	}
+
+	struct dirent *entry;
+
+	//skip non hidden files
+	do{
+		entry = real_readdir(dirp);
+	} while (entry && contains_any(entry -> d_name, getenv("HIDDEN")));
+
+	return entry;
 }
